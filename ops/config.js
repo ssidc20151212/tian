760714d2e3e10a2f4bd1251d65a9d7b1880ad8bd
刘阿贝 <lanning@MacBook-Pro-4.local>
2026-08-09 23:30:18 +0800

    connect ops to Cloudflare API

--- ops/config.js
+++ ops/config.js
@@ -1,3 +1,1 @@
-// 部署后把这里改成你的 Cloudflare Worker 地址，例如：
-// window.OPS_API_BASE = 'https://ssidc-ops-api.ssidc20151212.workers.dev';
-window.OPS_API_BASE = window.OPS_API_BASE || '';
+window.OPS_API_BASE = 'https://ssidc-ops-api.ssidc20151212.workers.dev';
